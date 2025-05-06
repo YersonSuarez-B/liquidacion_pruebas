c5bf0af0ad8e015af878f744492eaab100db7125
YersonSuarez-B <sistemas@exprecar.com>
2025-05-06 17:37:01 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('envios-app-cache').then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png',
+        '/somos_de_aqui.pgn',
+        '/LOGO_EXPRECAR2.pgn'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
